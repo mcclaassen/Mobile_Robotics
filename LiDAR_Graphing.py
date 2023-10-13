06b5a357c4b1dca9e3eb93b149a53f3cecabdb62
johnDanger <mc.claassen@outlook.com>
2023-10-13 16:21:11 -0700

    Added desktop scan for LiDAR

--- LiDAR_Graphing.py
+++ LiDAR_Graphing.py
@@ -4,6 +4,7 @@ import math
 import serial
 from matplotlib import pyplot as plt  # I think this caused Gtk warning when ran on RPi via ssh
 from objs_LiDAR import D300_LiDAR
+from matplotlib.cbook import get_sample_data
 
 #use to transfer file to RPi
 # $ sftp johndanger@raspberrypi.local
@@ -16,12 +17,12 @@ from objs_LiDAR import D300_LiDAR
 
 
 ##
-lidarObj = D300_LiDAR('RPi')
+lidarObj = D300_LiDAR('Mac')
 with lidarObj as lidarConn:
 
     packetDataByScan = {}
     packetInfoByScan = {}
-    while lidarObj.scan_counter < 1000 and lidarObj.timeout_counter < 10000:
+    while lidarObj.scan_counter < 500 and lidarObj.timeout_counter < 10000:
 
         lidarObj.read_packet()
         if not lidarObj.buffer:
@@ -58,16 +59,41 @@ for scanNum, scanInfo in packetInfoByScan.items():
     # angleBetweenPoints_speed = totalAngle_speed / (len(packetDataByScan[scanNum]) - 1)
     angleBetweenPoints_angle = totalAngle_angle / (len(packetDataByScan[scanNum]) - 1)
     for pointNum, (distance, intensity) in enumerate(packetDataByScan[scanNum]):
-        pointAngle = startAngle + angleBetweenPoints_angle*pointNum  #has shifted section around 0 degrees
-        # pointAngle = startAngle + angleBetweenPoints_speed*pointNum  #similar to above but doesn't have shifted section
-        #convert angle and distance into cartesian coordinates
-        allX.append(distance * math.cos(math.deg2rad(pointAngle)))
-        allY.append(distance * math.sin(math.deg2rad(pointAngle)))
+        if 30 < distance < 500:
 
+            pointAngle = startAngle + angleBetweenPoints_angle*pointNum  #has shifted section around 0 degrees
+            # pointAngle = startAngle + angleBetweenPoints_speed*pointNum  #similar to above but doesn't have shifted section
+            #convert angle and distance into cartesian coordinates
+            x = distance * math.cos(-math.radians(pointAngle))
+            y = distance * math.sin(-math.radians(pointAngle))
 
-plt.scatter(allX, allY, s=.1)
-plt.show()
+            allX.append(x)
+            allY.append(y)
 
+##
+
+fig, ax = plt.subplots(1)
+ax.set_title('LiDAR Scan of Lab')
+ax.set_xlabel('Distance (mm)')
+ax.set_ylabel('Distance (mm)')
+ax.scatter(allX, allY, s=.1)
+ax.scatter(0, 0, s=50)
+ax.set_xlim(-500, 500)
+ax.set_ylim(-500, 500)
+
+im = plt.imread(get_sample_data('/Users/jdanger/Desktop/Mobile_Robotics/desktop_LiDAR.jpg'))
+
+# Place the image in the upper-right corner of the figure
+#--------------------------------------------------------
+# We're specifying the position and size in _figure_ coordinates, so the image
+# will shrink/grow as the figure is resized. Remove "zorder=-1" to place the
+# image in front of the axes.
+# left, bottom, width, height
+newax = fig.add_axes([0.2, 0.33, 0.55, 0.55], anchor='NE')#, zorder=-1)
+newax.imshow(im, alpha=.3)
+newax.axis('off')
+
+fig.show()
 
 ##
 
